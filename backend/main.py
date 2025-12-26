c17b0b870975e698586085642813d21dcd869b05
Haritha Nallamilli <harithanallamilli9702@gmail.com>
2025-12-27 00:25:25 +0530

    changes in Dockerfile and main

--- backend/main.py
+++ backend/main.py
@@ -3,8 +3,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from config import get_settings
-from models.models import init_models
-from api.routes import router as api_router
+from backend.models import init_models
+from backend.api.routes import router as api_router
 
 settings = get_settings()
 
@@ -13,7 +13,7 @@ app = FastAPI(title="Sentiment Platform API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[f"http://localhost:{settings.frontend_port}", "*"],
+    allow_origins=[f"http://localhost:{settings.frontend_port}"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
